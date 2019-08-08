2593793def5c0f41f5915277e49c6db76e7abcd4
lmjing <lmjing1234@gmail.com>
2019-08-08 14:54:56 +0900

    Jest Install & Run Test Code

---
+++ chapter1/__tests__/sample.js
@@ -0,0 +1,75 @@
+//truthiness
+test('null', () => {
+    const n = null;
+    expect(n).toBeNull();
+    expect(n).toBeDefined();
+    expect(n).not.toBeUndefined();
+    expect(n).not.toBeTruthy();
+    expect(n).toBeFalsy();
+});
+
+test('zero', () => {
+    const z = 0;
+    expect(z).not.toBeNull();
+    expect(z).toBeDefined();
+    expect(z).not.toBeUndefined();
+    expect(z).not.toBeTruthy();
+    expect(z).toBeFalsy();
+});
+
+//Numbers
+test('two plus two', () => {
+    const value = 2 + 2;
+    expect(value).toBeGreaterThan(3);
+    expect(value).toBeGreaterThanOrEqual(3.5);
+    expect(value).toBeLessThan(5);
+    expect(value).toBeLessThanOrEqual(4.5);
+
+    // toBe and toEqual are equivalent for numbers
+    expect(value).toBe(4);
+    expect(value).toEqual(4);
+});
+
+test('adding floating point numbers', () => {
+    const value = 0.1 + 0.2;
+    //expect(value).toBe(0.3);           This won't work because of rounding error
+    expect(value).toBeCloseTo(0.3); // This works.
+});
+
+//Strings
+test('there is no I in team', () => {
+    expect('team').not.toMatch(/I/);
+});
+
+test('but there is a "stop" in Christoph', () => {
+    expect('Christoph').toMatch(/stop/);
+});
+
+//Arrays and iterables
+const shoppingList = [
+    'diapers',
+    'kleenex',
+    'trash bags',
+    'paper towels',
+    'beer',
+];
+
+test('the shopping list has beer on it', () => {
+    expect(shoppingList).toContain('beer');
+    expect(new Set(shoppingList)).toContain('beer');
+    expect(shoppingList).toHaveLength(5);
+});
+
+//Exceptions
+function compileAndroidCode() {
+    throw new SyntaxError('you are missing the statement');
+}
+
+test('compiling android goes as expected', () => {
+    expect(compileAndroidCode).toThrow(); // throw 여부만 확인
+    expect(compileAndroidCode).toThrow(SyntaxError);
+
+    // 에러 메세지나 메시지의 정규 표현식으로도 확인 가능
+    expect(compileAndroidCode).toThrow('you are missing the statement');
+    expect(compileAndroidCode).toThrow(/statement/);
+});
