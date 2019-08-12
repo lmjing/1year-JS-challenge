1c84b1f2233444bb246c5c9abc3d1fea5248e194
lmjing <lmjing1234@gmail.com>
2019-08-12 20:11:04 +0900

    data 생성 후 리스트innerHTML로 선언

---
+++ chapter1/src/controller.js
@@ -0,0 +1,52 @@
+// data.json으로 따로 빼서 import 하려 했지만 잘 안됨
+var data = [
+    {
+        "key": "A",
+        "sound": "clap"
+    },
+    {
+        "key": "S",
+        "sound": "hihat"
+    },
+    {
+        "key": "D",
+        "sound": "kick"
+    },
+    {
+        "key": "F",
+        "sound": "openhat"
+    },
+    {
+        "key": "G",
+        "sound": "boom"
+    },
+    {
+        "key": "H",
+        "sound": "ride"
+    },
+    {
+        "key": "J",
+        "sound": "snare"
+    },
+    {
+        "key": "K",
+        "sound": "tom"
+    },
+    {
+        "key": "L",
+        "sound": "tink"
+    }
+];
+
+let contents = (data) => {
+    let template = '';
+    for (let i=0; i<data.length; i++) {
+        template += `
+        <div class="${data[i].key}">${data[i].key}</div>
+        `;
+    }
+    return template;
+};
+
+let content = document.getElementById("content");
+content.innerHTML = contents(data);
