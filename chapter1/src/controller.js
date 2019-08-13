c7ddf2676fec8f10b4d2212419861024c3c7dd9f
lmjing <lmjing1234@gmail.com>
2019-08-13 15:03:46 +0900

    키보드 눌렸을 때 box UI 변경

--- chapter1/src/controller.js
+++ chapter1/src/controller.js
@@ -20,8 +20,8 @@ let contents = (data) => {
     let template = '';
     for (let key in data) {
         template += `
-        <div class="box">
-            <div class="box--key">${key}</div>
+        <div class="box ${key}-box">
+            <kbd class="box--key">${key}</kbd>
             <div class="box--sound">${data[key]}</div>
         </div>
         `;
@@ -29,15 +29,40 @@ let contents = (data) => {
     return template;
 };
 
-// 참고 : https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent
+/*
+ 참고
+ 1) 키보드 이벤트 - https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent
+ 2) 오디오 재생 - https://hashcode.co.kr/questions/1479/%EC%9E%90%EB%B0%94%EC%8A%A4%ED%81%AC%EB%A6%BD%ED%8A%B8%EC%97%90%EC%84%9C-%EC%9D%8C%EC%95%85-%EC%9E%AC%EC%83%9D%ED%95%98%EB%8A%94%EB%B2%95
+ 3) class 추가 - http://blim.co.kr/archives/160
+  */
 let setKeyboardEvent = () => {
     document.addEventListener("keydown", event => {
         let keyName = event.key.toUpperCase();
         let audio = new Audio(`../asset/sound/${data[keyName]}.wav`);
-        audio.play();
+        let boxName = `${keyName}-box`;
+
+        audio.play()
+            .then(() => {
+                // Question : 왜 여기서 keyName에 접근하면 null이 뜨는지??
+                let box = document.querySelector(`.${boxName}`);
+                box.classList.add("box--select");
+            })
+            .catch((error) => {
+                console.log(error);
+            });
 
         keyName = null;
         audio = null;
+    });
+
+    document.addEventListener("keyup", event => {
+        let keyName = event.key.toUpperCase();
+        let boxName = `${keyName}-box`;
+
+        let box = document.querySelector(`.${boxName}`);
+        box.classList.remove("box--select");
+
+        keyName = null;
     });
 };
 
