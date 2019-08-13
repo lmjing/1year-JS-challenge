6ce77c7caef22c85bc24440895f9e76552dc5f8d
lmjing <lmjing1234@gmail.com>
2019-08-13 14:10:54 +0900

    데이터 구조 변경 및 이에따른 코드 리팩토링(간소화)

--- chapter1/src/controller.js
+++ chapter1/src/controller.js
@@ -1,65 +1,43 @@
+/**
+ * @author 이미정
+ * @file controller.js
+ * @description 데이터 설정 및 이벤트 컨트롤러
+ */
 // data.json으로 따로 빼서 import 하려 했지만 잘 안됨
-var data = [
-    {
-        "key": "A",
-        "sound": "clap"
-    },
-    {
-        "key": "S",
-        "sound": "hihat"
-    },
-    {
-        "key": "D",
-        "sound": "kick"
-    },
-    {
-        "key": "F",
-        "sound": "openhat"
-    },
-    {
-        "key": "G",
-        "sound": "boom"
-    },
-    {
-        "key": "H",
-        "sound": "ride"
-    },
-    {
-        "key": "J",
-        "sound": "snare"
-    },
-    {
-        "key": "K",
-        "sound": "tom"
-    },
-    {
-        "key": "L",
-        "sound": "tink"
-    }
-];
+const data = {
+    "A": "clap",
+    "S": "hihat",
+    "D": "kick",
+    "F": "openhat",
+    "G": "boom",
+    "H": "ride",
+    "J": "snare",
+    "K": "tom",
+    "L": "tink",
+};
 
 let contents = (data) => {
     let template = '';
-    for (let i=0; i<data.length; i++) {
+    for (let key in data) {
         template += `
         <div class="box">
-            <div class="box--key">${data[i].key}</div>
-            <div class="box--sound">${data[i].sound}</div>
+            <div class="box--key">${key}</div>
+            <div class="box--sound">${data[key]}</div>
         </div>
         `;
     }
     return template;
 };
 
-// https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent
+// 참고 : https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent
 let setKeyboardEvent = () => {
     document.addEventListener("keydown", event => {
         let keyName = event.key.toUpperCase();
-        let item = data.find(item => {
-            return item.key === keyName;
-        });
-        let audio = new Audio(`../asset/sound/${item.sound}.wav`);
+        let audio = new Audio(`../asset/sound/${data[keyName]}.wav`);
         audio.play();
+
+        keyName = null;
+        audio = null;
     });
 };
 
@@ -69,3 +47,5 @@ content.innerHTML = contents(data);
 setKeyboardEvent();
 
 content = null;
+contents = null;
+setKeyboardEvent = null;
