8f16b181e98a66d771068e863bc5dc544f5862ef
lmjing <lmjing1234@gmail.com>
2019-09-06 13:02:12 +0900

    초침 이동 구현

--- chapter2/src/timer.js
+++ chapter2/src/timer.js
@@ -1,7 +1,16 @@
+let setSecondHand = (second) => {
+    let secondHand = document.querySelector('.second');
+    let angle = second * 6;
+    console.log(`${second}초의 각도는 ${angle}`);
+    secondHand.style.transform = `rotate(${angle}deg)`;
+};
+
 let setTime = () => {
-    let now = Date();
+    let now = new Date();
     let el = document.getElementById('time');
     el.innerText = now;
+
+    setSecondHand(now.getSeconds());
 
     now = null; el = null;
 };
