38cde31d5a7ef292c97d863ecdd551ee2a9fc58b
lmjing <lmjing1234@gmail.com>
2019-09-15 15:10:26 +0900

    불필요한 파일 삭제

--- chapter2/src/timer.js
+++ chapter2/src/timer.js
@@ -1,86 +1,56 @@
-let beforeTime = null;
-
-let moveHourHand = (hour) => {
-    let hourHand = document.querySelector('.hour');
-    let angle = (hour % 12) * 30;
-    hourHand.style.transform = `rotate(${angle}deg)`;
-    if (beforeTime !== null)
-        setTransition(hourHand, 3600);
-
-    hourHand = null; angle = null;
+let timeToAngle = (type, time, gone = 0) => {
+    let originAngle = type === "hour" ?  (time % 12) * 30 : time * 6;
+    let goneAngle =  (type === "hour" ? 0.2 : 0.1) * gone;
+    return originAngle + goneAngle;
 };
 
-let moveMinuteHand = (minute) => {
-    let minuteHand = document.querySelector('.minute');
-    let angle = minute * 6;
-    minuteHand.style.transform = `rotate(${angle}deg)`;
-    if (beforeTime !== null)
-        setTransition(minuteHand, 60);
+let moveHand = (type, angle) => {
+    let el = document.querySelector(`.${type}`);
+    el.style.transform = `rotate(${angle}deg)`;
 
-    minuteHand = null; angle = null;
+    el = null; angle = null;
 };
 
-let moveSecondHand = (second) => {
-    let secondHand = document.querySelector('.second');
-    let angle = second * 6;
-    secondHand.style.transform = `rotate(${angle}deg)`;
-
-    secondHand = null; angle = null;
-};
-
-let moveMinuteCheck = (minute) => {
-    let minuteHand = document.querySelector('.minute-check');
-    let test = document.getElementById('test');
-    let angle = minute * 6;
-    minuteHand.style.transform = `rotate(${angle}deg)`;
-    test.style.transform = `rotate(${angle + 6}deg)`;
-
-    minuteHand = null; angle = null;
-};
-let moveHourCheck = (hour) => {
-    let hourHand = document.querySelector('.hour-check');
-    let angle = (hour % 12) * 30;
-    hourHand.style.transform = `rotate(${angle}deg)`;
-
-    hourHand = null; angle = null;
-};
-let setTime = () => {
+let setInitTime = () => {
     let now = new Date();
     let hour = now.getHours();
     let minute = now.getMinutes();
-
-    moveMinuteCheck(30);
-
-    let timeEl = document.getElementById('time');
-    timeEl.innerText = now;
-
-    moveSecondHand(now.getSeconds());
-    if (beforeTime === null) {
-        moveHourHand(hour);
-        moveMinuteHand(minute);
-        beforeTime = now;
-        return;
-    }
-
-    if (beforeTime.getHours() !== hour) {
-        moveHourHand(hour);
-    }
-    if (beforeTime.getMinutes() !== minute) {
-        console.log(`분이 달라졌어요! ${beforeTime.getMinutes()} => ${minute}`);
-        moveMinuteHand(minute);
-    }
-
-    beforeTime = now;
-    now = null;
+    let second = now.getSeconds();
+    moveHand('hour', timeToAngle('hour', hour, minute));
+    moveHand('minute', timeToAngle('minute', minute, second));
+    moveHand('second', timeToAngle('seconde', second));
 };
 
-let setTransition = (el, time) => {
-  el.style.transition = `transform ${time}s`;
-  el.style.webkitTransition = `transform ${time}s`;
-};
-let startClock = () => {
-    setTime();
-    setInterval(setTime, 1000);
+// 다음 시간을 향해 트랜지션 적용
+let setNextTime = (type, now) => {
+    // 일자 더하기 (지금 그냥 셋팅 해버림)
+    let nextTime = new Date();
+    if (type === 'minute') nextTime.setMinutes(now.getMinutes() + 1);
+    else nextTime.setHours(now.getHours() + 1);
+    let angle = timeToAngle(type, type === 'minute' ? nextTime.getMinutes() : nextTime.getHours());
+    moveHand(type, angle);
+    setTransition(type, type === 'minute' ? 60 : 3600);
 };
 
-startClock();
+let timeUpdate = () => {
+    let now = new Date();
+    // 초는 바로 현재 시간 가르킴
+    moveHand('second', timeToAngle('second', now.getSeconds()));
+    // 분, 시는 다음 시간 가르키며, 트랜지션 적용
+    setNextTime('minute', now);
+    setNextTime('hour', now);
+};
+
+let setTransition = (type, time) => {
+    let el = document.querySelector(`.${type}`);
+    el.style.transition = `transform ${time}s`;
+    el.style.webkitTransition = `transform ${time}s`;
+};
+
+// 맨 처음 시간 셋팅(해당 값 기준으로 서서히 트랜지션 적용해 이동될 예정)
+setInitTime();
+
+// 여기 남은 시간 계산해서 해당 시간만큼 transition 적용해야함.
+setTimeout(setTransition, 1000, 'minute', 60);
+setTimeout(setTransition, 1000, 'hour', 3600);
+setInterval(timeUpdate, 1000);
