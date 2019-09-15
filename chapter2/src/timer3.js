ec41888ea48f35ff47aad4d334d006ece50c53cd
lmjing <lmjing1234@gmail.com>
2019-09-15 15:06:46 +0900

    시간 초기 셋팅 지난 시간 각도 포함 + 임의 분침으로 비교(테스트) 완료

--- chapter2/src/timer3.js
+++ chapter2/src/timer3.js
@@ -1,10 +1,11 @@
-let timeToAngle = (type, time) => {
-    return type === "hour" ?  (time % 12) * 30 : time * 6;
+let timeToAngle = (type, time, gone = 0) => {
+    let originAngle = type === "hour" ?  (time % 12) * 30 : time * 6;
+    let goneAngle =  (type === "hour" ? 0.2 : 0.1) * gone;
+    return originAngle + goneAngle;
 };
 
-let moveHand = (type, time) => {
+let moveHand = (type, angle) => {
     let el = document.querySelector(`.${type}`);
-    let angle = timeToAngle(type, time);
     el.style.transform = `rotate(${angle}deg)`;
 
     el = null; angle = null;
@@ -12,9 +13,12 @@ let moveHand = (type, time) => {
 
 let setInitTime = () => {
     let now = new Date();
-    moveHand('hour', now.getHours());
-    moveHand('minute', now.getMinutes());
-    moveHand('second', now.getSeconds());
+    let hour = now.getHours();
+    let minute = now.getMinutes();
+    let second = now.getSeconds();
+    moveHand('hour', timeToAngle('hour', hour, minute));
+    moveHand('minute', timeToAngle('minute', minute, second));
+    moveHand('second', timeToAngle('seconde', second));
 };
 
 // 다음 시간을 향해 트랜지션 적용
@@ -23,14 +27,15 @@ let setNextTime = (type, now) => {
     let nextTime = new Date();
     if (type === 'minute') nextTime.setMinutes(now.getMinutes() + 1);
     else nextTime.setHours(now.getHours() + 1);
-    moveHand(type, type === 'minute' ? nextTime.getMinutes() : nextTime.getHours());
+    let angle = timeToAngle(type, type === 'minute' ? nextTime.getMinutes() : nextTime.getHours());
+    moveHand(type, angle);
     setTransition(type, type === 'minute' ? 60 : 3600);
 };
 
 let timeUpdate = () => {
     let now = new Date();
     // 초는 바로 현재 시간 가르킴
-    moveHand('second', now.getSeconds());
+    moveHand('second', timeToAngle('second', now.getSeconds()));
     // 분, 시는 다음 시간 가르키며, 트랜지션 적용
     setNextTime('minute', now);
     setNextTime('hour', now);
@@ -44,8 +49,20 @@ let setTransition = (type, time) => {
     el.style.transition = `transform ${time}s`;
     el.style.webkitTransition = `transform ${time}s`;
 };
+
+
+// ㅅㅣ간 체크용
+let check1 = document.querySelector(`.check-minute`);
+let check2 = document.querySelector(`.check-m`);
+
+let angle1 = timeToAngle('minute', 4);
+let angle2 = timeToAngle('minute', 5);
+check1.style.transform = `rotate(${angle1}deg)`;
+check2.style.transform = `rotate(${angle2}deg)`;
+
 // 맨 처음 시간 셋팅(해당 값 기준으로 서서히 트랜지션 적용해 이동될 예정)
 setInitTime();
+
 // 여기 남은 시간 계산해서 해당 시간만큼 transition 적용해야함.
 setTimeout(setTransition, 1000, 'minute', 60);
 setTimeout(setTransition, 1000, 'hour', 3600);
