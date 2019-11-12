fd99f196ca3678d7c3a4c46610f01b9146cd72ee
lmjing <lmjing1234@gmail.com>
2019-11-12 17:45:37 +0900

    코드 간결화

--- chapter11-Custom Video Player/scripts.js
+++ chapter11-Custom Video Player/scripts.js
@@ -18,11 +18,8 @@ function updateButton() {
     toggle.innerText = button;
     button = null;
 }
-function updateSound() {
-    video.volume = ranges[0].value;
-}
-function updateSpeed() {
-    video.playbackRate = ranges[1].value;
+function updateRange() {
+    video[this.name] = this.value;
 }
 function updateProgressBar(e) {
     let percent = 0;
@@ -39,8 +36,6 @@ function updateProgressBar(e) {
  따라서, changePlayTime의 메소드에서 e.offsetX는 progress의 시작점부터 측정되는 X 값이다.
   */
 function changePlayTime(e) {
-    console.log(e);
-    e.preventDefault();
     let playTime = (e.offsetX / progress.offsetWidth) * video.duration;
     video.currentTime = playTime;  // chrome에서 몇몇 안되는 현상 발생
     console.log(playTime);
@@ -58,17 +53,16 @@ function skip() {
 video.addEventListener('click', togglePlay);
 video.addEventListener('click', updateButton);
 video.addEventListener('timeupdate', updateProgressBar);
+video.addEventListener('playing', (e) => console.log(e));
 
 toggle.addEventListener('click', togglePlay);
 toggle.addEventListener('click', updateButton);
 
-ranges[0].addEventListener('input', updateSound);
-ranges[1].addEventListener('input', updateSpeed);
+ranges.forEach(range => range.addEventListener('input', updateRange));
 
 progress.addEventListener('click', changePlayTime);
 progress.addEventListener('mousemove', (e) => { mousedown && updateProgressBar(e)});
 progress.addEventListener('mousedown', () => { mousedown = true });
 progress.addEventListener('mouseup', changePlayTime);
 
-// skipButtons.forEach(button => button.addEventListener('click', skip));
-skipButtons[1].addEventListener('click', skip);
+skipButtons.forEach(button => button.addEventListener('click', skip));
